86a3dc7493e32814b2d50836bc9dcac2ba9b1688
javiercolodro <javiercolodro@gmail.com>
2026-08-04 14:51:19 -0300

    Initial commit

---
+++ test/stepfunctions-destination-ai.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as StepfunctionsDestinationAi from '../lib/stepfunctions-destination-ai-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/stepfunctions-destination-ai-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new StepfunctionsDestinationAi.StepfunctionsDestinationAiStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
